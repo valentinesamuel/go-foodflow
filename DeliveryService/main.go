509a6bb0dee128dbd733509d1cdcca896fc96810
valentinesamuel <valentinesamuel2580@gmail.com>
2024-07-09 02:16:52 +0100

    feat: tidied go mods

--- DeliveryService/main.go
+++ DeliveryService/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"DeliveryService/db"
+	"DeliveryService/routes"
 	"fmt"
 	"log"
 	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+
+	"go-foodflow/utilities/models"
 )
 
 func main() {
 	db.ConnectDeliveryDB()
+
+	db.DeliveryDB.AutoMigrate(&models.DeliveryTracking{})
+
+	server := gin.Default()
+
+	routes.RegisterRoutes(server)
+
 	dbName := getEnvVar("DELIVERY_DB_NAME")
+	port := getEnvVar("PORT")
 
 	fmt.Println("Hello Delivery! 🚀=>", dbName)
+
+	server.Run(":" + port)
 }
 
 func getEnvVar(key string) string {
--- utilities/go.mod
+++ utilities/go.mod
@@ -1,4 +1,4 @@
-module utils
+module go-foodlow/utilities
 
 go 1.22.4
 
